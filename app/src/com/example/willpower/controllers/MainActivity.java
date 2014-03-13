4de4b2d297d43ba220cc1f448be08ca8bdb8d9bb
Yuxin Zhong <yuxinzhong@icloud.com>
2014-03-12 23:09:52 -0700

    Add click effect to image buttons

--- app/src/com/example/willpower/controllers/MainActivity.java
+++ app/src/com/example/willpower/controllers/MainActivity.java
@@ -5,45 +5,14 @@ import com.example.willpower.controllers.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private ImageButton ib1,ib2,ib3,ib4;
-	private void setupUI(){
-		ib1=(ImageButton) findViewById(R.id.ImageButtonGame1);
-		ib2=(ImageButton) findViewById(R.id.ImageButtonGame2);
-		ib3=(ImageButton) findViewById(R.id.ImageButtonGame3);
-		ib4=(ImageButton) findViewById(R.id.ImageButtonGame4);
-		ib1.setOnClickListener(new View.OnClickListener() {
-			@Override
-			//Yuxin Override this method
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "Button1 is clicked, please override this method to activate game1Activity", 3000);
-			}
-		});
-		ib2.setOnClickListener(new View.OnClickListener() {
-			@Override
-			//Lai Override this method
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "Button2 is clicked, please override this method to activate game2Activity", 3000);
-			}
-		});
-		ib3.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "Button3 is clicked, please override this method to activate game3Activity", 3000);
-			}
-		});
-		ib4.setOnClickListener(new View.OnClickListener() {
-			@Override
-			//Yao Override this method
-			public void onClick(View v) {
-				Toast.makeText(MainActivity.this, "Button4 is clicked, please override this method to activate game4Activity", 3000);
-			}
-		});
-	}
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -57,5 +26,69 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	
+	private void setupUI(){
+		ib1=(ImageButton) findViewById(R.id.ImageButtonGame1);
+		ib2=(ImageButton) findViewById(R.id.ImageButtonGame2);
+		ib3=(ImageButton) findViewById(R.id.ImageButtonGame3);
+		ib4=(ImageButton) findViewById(R.id.ImageButtonGame4);
+		View.OnTouchListener onTouchListener=new View.OnTouchListener() {
+			//add touch effect to the imageButtons
+			//refer to the solution from http://stackoverflow.com/questions/4617898/how-can-i-give-imageview-click-effect-like-a-button-in-android
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				// TODO Auto-generated method stub
+		           switch (event.getAction()) {
+	                case MotionEvent.ACTION_DOWN: {
+	                    ImageView view = (ImageView) v;
+	                    //overlay is black with transparency of 0x77 (119)
+	                    view.getDrawable().setColorFilter(0x77000000,android.graphics.PorterDuff.Mode.SRC_ATOP);
+	                    view.invalidate();
+	                    break;
+	                }
+	                case MotionEvent.ACTION_UP:
+	                case MotionEvent.ACTION_CANCEL: {
+	                    ImageView view = (ImageView) v;
+	                    //clear the overlay
+	                    view.getDrawable().clearColorFilter();
+	                    view.invalidate();
+	                    break;
+	                }
+	            }    
+                return false;
+           }
+		};
+		ib1.setOnTouchListener(onTouchListener);
+		ib2.setOnTouchListener(onTouchListener);
+		ib3.setOnTouchListener(onTouchListener);
+		ib4.setOnTouchListener(onTouchListener);
 
+		ib1.setOnClickListener(new View.OnClickListener() {
+			@Override
+			//Yuxin Override this method
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Button1 is clicked, please override this method to activate game1Activity", 2000).show();
+			}
+		});
+		ib2.setOnClickListener(new View.OnClickListener() {
+			@Override
+			//Lai Override this method
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Button2 is clicked, please override this method to activate game2Activity", 2000).show();
+			}
+		});
+		ib3.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Button3 is clicked, please override this method to activate game3Activity", 2000).show();
+			}
+		});
+		ib4.setOnClickListener(new View.OnClickListener() {
+			@Override
+			//Yao Override this method
+			public void onClick(View v) {
+				Toast.makeText(MainActivity.this, "Button4 is clicked, please override this method to activate game4Activity", 2000).show();
+			}
+		});
+	}
 }
