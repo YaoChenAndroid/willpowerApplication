b982eb383bce8e836c269029c215c20345568775
Yuxin Zhong <yuxinzhong@icloud.com>
2014-03-22 19:48:04 -0700

    BrainActivity basic version 1.0

--- app/src/com/example/willpower/yuxin/models/Question.java
+++ app/src/com/example/willpower/yuxin/models/Question.java
@@ -1,9 +1,24 @@
 package com.example.willpower.yuxin.models;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
+
 public class Question {
+	private static String[] words=new String[]{"purple","green","blue","pink","brown","red","orange","yellow","white","black"};
+	private static String[] colors=new String[]{"#7e1e9c","#15b01a","#0343df","#ff81c0","#653700","#e50000","#f97306","#ffff14","#ffffff","#000000"};
+	private static String[] questions=new String[]{"choose the meaning of the word.","choose the color of the word."};
+	private static HashMap<String, String> wordToColor=new HashMap<String,String>(){{
+		for(int i=0;i<words.length;i++){
+			put(words[i], colors[i]);
+		}
+	}};
+	
 	private String word;
 	private String wordColor;
-	private String question;
+	private int questionType;
 	private String[] colorOptions;
 	
 	public String getWord() {
@@ -15,7 +30,7 @@ public class Question {
 	}
 
 	public String getQuestion() {
-		return question;
+		return questions[questionType];
 	}
 
 	public String[] getColorOptions() {
@@ -23,15 +38,33 @@ public class Question {
 	}
 	
 	public boolean isCorrect(int i){
-		return true;
+		if (questionType==0) {
+			String meaningColor=wordToColor.get(word);
+			if(colorOptions[i].equals(meaningColor)) return true;
+			else return false;
+		} else {
+			if(colorOptions[i].equals(wordColor)) return true;
+			else return false;
+		}
 	}
 	
 	public static Question newQuestion() {
 		Question q=new Question();
-		q.word="Yellow";
-		q.wordColor="#ffffff";
-		q.question="choose the meaning of the word.";
-		q.colorOptions=new String[]{"#F7F700","#000000","#ffffff"};
+		int i,j,k,l;
+		i=(int)(Math.random()*words.length);
+		j=(int)(Math.random()*words.length);
+		for(;;){
+			k=(int)(Math.random()*words.length);
+			if((k!=i)&&(k!=j)) break;
+		}
+		l=(int)(Math.random()*questions.length);
+		
+		q.word=words[i];
+		q.wordColor=colors[j];
+		q.questionType=l;
+		List list=Arrays.asList(new String[]{colors[i],colors[j],colors[k]});
+		Collections.shuffle(list);
+		q.colorOptions=(String[]) list.toArray(new String[0]);
 		return q;
 	}
 	
