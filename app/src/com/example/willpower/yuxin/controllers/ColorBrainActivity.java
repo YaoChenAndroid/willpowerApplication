c0354cfd22c4b397cf297a036d02ef61d7c7a8ae
Yuxin Zhong <yuxinzhong@icloud.com>
2014-03-22 19:11:43 -0700

    BrainActivity UI setup

--- app/src/com/example/willpower/yuxin/controllers/ColorBrainActivity.java
+++ app/src/com/example/willpower/yuxin/controllers/ColorBrainActivity.java
@@ -1,14 +1,75 @@
 package com.example.willpower.yuxin.controllers;
 
 import com.example.willpower.controllers.R;
+import com.example.willpower.yuxin.models.Question;
+
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ColorBrainActivity extends Activity {
+	TextView cbtv1,cbtv2,cbtv3,cbtv4,cbtv5,cbtv6;
+	Button cbbtn1,cbbtn2,cbbtn3;
+	Button[] cbbtns;
+	Question currentQuestion;
+	int right=0;
+	int wrong=0;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_colorbrain_yuxin);
+		currentQuestion=Question.newQuestion();
+		setupUI();
+		refreshUI();
+	}
+
+	
+	private void refreshUI() {
+		cbtv1.setText(currentQuestion.getWord());
+		cbtv1.setTextColor(Color.parseColor(currentQuestion.getWordColor()));
+		cbtv2.setText(currentQuestion.getQuestion());
+		for(int i=0;i<3;i++){
+			cbbtns[i].setBackgroundColor(Color.parseColor(currentQuestion.getColorOptions()[i]));
+		}
+		cbtv4.setText(right+"");
+		cbtv6.setText(wrong+"");
+	}
+
+	private void setupUI() {
+		cbtv1=(TextView) findViewById(R.id.cbtv1);
+		cbtv2=(TextView) findViewById(R.id.cbtv2);
+		cbtv3=(TextView) findViewById(R.id.cbtv3);
+		cbtv4=(TextView) findViewById(R.id.cbtv4);
+		cbtv5=(TextView) findViewById(R.id.cbtv5);
+		cbtv6=(TextView) findViewById(R.id.cbtv6);
+		
+		cbbtn1=(Button) findViewById(R.id.cbbtn1);
+		cbbtn2=(Button) findViewById(R.id.cbbtn2);
+		cbbtn3=(Button) findViewById(R.id.cbbtn3);
+		
+		cbbtns=new Button[]{cbbtn1,cbbtn2,cbbtn3};
+		
+		View.OnClickListener onClickListener1=new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Button b=(Button) v;
+				int i=0;
+				for(;i<3;i++) if(b==cbbtns[i]) break;
+				if (currentQuestion.isCorrect(i)) {
+					right++;
+				} else {
+					wrong++;
+				}
+				currentQuestion=Question.newQuestion();
+				refreshUI();				
+			}
+		};
+		
+		cbbtn1.setOnClickListener(onClickListener1);
+		cbbtn2.setOnClickListener(onClickListener1);
+		cbbtn3.setOnClickListener(onClickListener1);
 	}
 }
--- app/src/com/example/willpower/yuxin/models/Question.java
+++ app/src/com/example/willpower/yuxin/models/Question.java
@@ -1,7 +1,39 @@
 package com.example.willpower.yuxin.models;
 
 public class Question {
-	public static Question newQuestion() {
-		return null;
+	private String word;
+	private String wordColor;
+	private String question;
+	private String[] colorOptions;
+	
+	public String getWord() {
+		return word;
 	}
+
+	public String getWordColor() {
+		return wordColor;
+	}
+
+	public String getQuestion() {
+		return question;
+	}
+
+	public String[] getColorOptions() {
+		return colorOptions;
+	}
+	
+	public boolean isCorrect(int i){
+		return true;
+	}
+	
+	public static Question newQuestion() {
+		Question q=new Question();
+		q.word="Yellow";
+		q.wordColor="#ffffff";
+		q.question="choose the meaning of the word.";
+		q.colorOptions=new String[]{"#F7F700","#000000","#ffffff"};
+		return q;
+	}
+	
+	private Question(){}
 }
