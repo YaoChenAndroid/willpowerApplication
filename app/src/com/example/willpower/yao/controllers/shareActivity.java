5a8232a6f4dbc6dea1bf95d144396fec455a9079
YaoChenAndroid <sabrina0911@live.cn>
2014-04-04 01:33:50 -0400

    add logical part of shareActivity

--- app/src/com/example/willpower/yao/controllers/shareActivity.java
+++ app/src/com/example/willpower/yao/controllers/shareActivity.java
@@ -1,5 +1,28 @@
 package com.example.willpower.yao.controllers;
 
-public class shareActivity {
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.willpower.controllers.R;
+public class shareActivity extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_social_share_yao);
+		// TODO - fill in here	
+		Button buttonShare = (Button) findViewById(R.id.buttonShare);
+		buttonShare.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+		});
+
+
+	}
 }
