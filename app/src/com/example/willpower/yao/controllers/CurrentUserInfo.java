cd333ab79333942c47b5d8a72938d1cca4c48db6
sunny <sabrina0911@live.cn>
2014-05-10 00:13:35 -0400

    add comment for the currentUserInfo class

--- app/src/com/example/willpower/yao/controllers/CurrentUserInfo.java
+++ app/src/com/example/willpower/yao/controllers/CurrentUserInfo.java
@@ -1,11 +1,13 @@
 package com.example.willpower.yao.controllers;
-
+//used in user system to record current user information
+// there are only one current user at one time
 public class CurrentUserInfo {
 	private static CurrentUserInfo currentUser = null;
 	public String userName = "";
 	public String UserId = "";
 	public String UserGoal = "";
 	protected CurrentUserInfo(){};
+	//single modle to make sure only one current user
 	public static synchronized CurrentUserInfo getInstance(){
 		if(null == currentUser){
 			currentUser = new CurrentUserInfo();
